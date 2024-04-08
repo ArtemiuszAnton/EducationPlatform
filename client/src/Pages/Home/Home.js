1f64646a5e59f3edd32bfa8e8792d124f569af32
ArtemiuszAnton <elvispresley9122@gmail.com>
2024-04-08 23:22:54 +0300

    add footer (not final)

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -12,7 +12,7 @@ function Home() {
 
         <div className={style.border_line}></div>
 
-        <div className={style.main}>
+        <main>
             <div className={style.first_block}>
 
                 <div className={style.left_side}>
@@ -65,7 +65,36 @@ function Home() {
                 </div>
             </div>
 
-        </div>
+        </main>
+
+        <footer>
+            <div className={style.links}>
+                <div className={style.pages}>
+                    <p>Home</p>
+                    <p>Textbook</p>
+                    <p>Statistics</p>
+                    <p>Sprint</p>
+                </div>
+                <div className={style.lectures}>
+                    <p>Alex</p>
+                    <p>Gabriel</p>
+                    <p>Marcus</p>
+                </div>
+            </div>
+
+            <div className={style.border_line}></div>
+
+            <div className={style.under_info}>
+                <div className={style.soc_networks}>
+                    <div className={style.github}></div>
+                    <div className={style.gt}></div>
+                    <div className={style.yuotube}></div>
+                </div>
+                <div className={style.trade_mark}>
+                    <p>©2021 Hschool. Project for <span>Hschool</span>.</p>
+                </div>
+            </div>
+        </footer>
 
     </div>
 }
