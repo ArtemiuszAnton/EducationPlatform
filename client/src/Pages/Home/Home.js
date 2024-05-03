a9fafeba138b7c48b73160c08b3642feed08f814
ArtemiuszAnton <elvispresley9122@gmail.com>
2024-05-03 22:41:02 +0200

    refactoring code

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -64,35 +64,6 @@ function Home() {
 
         <Footer/>
 
-        {/* <footer>
-            <div className={style.links}>
-                <div className={style.pages}>
-                    <p>Home</p>
-                    <p>Textbook</p>
-                    <p>Statistics</p>
-                    <p>Sprint</p>
-                </div>
-                <div className={style.lectures}>
-                    <p>Alex</p>
-                    <p>Gabriel</p>
-                    <p>Marcus</p>
-                </div>
-            </div>
-
-            <div className={style.border_line}></div>
-
-            <div className={style.under_info}>
-                <div className={style.soc_networks}>
-                    <div className={style.github}></div>
-                    <div className={style.gt}></div>
-                    <div className={style.youtube}></div>
-                </div>
-                <div className={style.trade_mark}>
-                    <p>©2021 Hschool. Project for <span>Hschool</span>.</p>
-                </div>
-            </div>
-        </footer>  */}
-
     </div>
 }
 
